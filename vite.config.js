76dbbf23f3d91767569423727329b20a55eedfc9
danaNB01 <baeisa.dana1@gmail.com>
2025-10-05 00:55:04 +0300

    ready for vercel

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/weather-app-react/",
+  // base: "/weather-app-react/",
 });
